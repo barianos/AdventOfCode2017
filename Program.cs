7a4fa2db60a9dc7307064731adf47e695b3b50db
Anastasios Barianos <barianos@outlook.com>
2017-12-08 00:41:26 +0200

    Changed Architecture

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            DayOne d1 = new DayOne();
+            return;
+        }
+    }
+}
